b3375b4c48e829a9ba7bb68a721ee4ddf52d1959
josiah-singleton <91489627+josiah-singleton@users.noreply.github.com>
2022-04-29 09:50:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const object = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+}
+localStorage.setItem('object','age') 
+localStorage.getItem("age")
+console.log(object)
